7c5a13dd1d501ae8d53a0beeaa018da1f14872ee
David Grant <davidttgg@gmail.com>
2022-11-22 19:29:59 +0000

    Add uniform stellar grid builds.

---
+++ grid_build/view_comparison.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Stellar parameters.
+M_H = -1.0
+Teff = 5000
+logg = 4.0
+
+# Grids.
+grids = ['kurucz', 'stagger', 'mps1', 'mps2']
+
+# Location.
+mu = 0.5
+
+# Plot each available spectrum.
+for g in grids:
+
+    file_name = os.path.join(
+        "../data", g, "MH{}".format(M_H),
+        "teff{}".format(Teff), "logg{}".format(logg),
+        "{}_spectra.dat".format(g))
+
+    try:
+        mu_data = np.loadtxt(file_name, skiprows=1, max_rows=1)
+        stellar_data = np.loadtxt(file_name, skiprows=2)
+    except FileNotFoundError as err:
+        print('Model corresponding to Teff={}, logg={}, and M_H={} '
+              'does not exist in the {} stellar models.'.format(
+               Teff, logg, M_H, g))
+        continue
+
+    mu_idx = np.argmin(np.abs(mu_data - mu))
+    plt.plot(stellar_data[:, 0], stellar_data[:, 1 + mu_idx], label=g)
+
+plt.xlim(0, 35000)
+plt.legend()
+plt.xlabel('Wavelength / $\AA$')
+plt.ylabel('Intensity / $n_{\gamma} s^{-1} cm^{-2} {\AA}^{-1}$')
+plt.show()
